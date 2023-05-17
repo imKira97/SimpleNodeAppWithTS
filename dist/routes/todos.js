197661338f1ce5ea096a66ef6cb602832900e6cf
imKira97 <codingshiva97>
2023-05-17 20:35:32 +0530

    change in file structure

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -8,18 +8,21 @@ router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post('/todo', (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newTodo);
     res.status(201).json({ message: 'todo added', todo: newTodo, todos: todos });
 });
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const body = req.body;
+    const params = req.params;
+    const tid = params.todoId;
     const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
     if (todoIndex >= 0) {
-        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
         //we write return bcoz we want to return from here and dont want to send the res of else block
         return res.status(200).json({ message: 'updated todo', todos: todos });
     }
@@ -27,7 +30,8 @@ router.put('/todo/:todoId', (req, res, next) => {
 });
 router.delete('/todo/:todoId', (req, res, next) => {
     //here we will filter out the todo which we want to delete 
-    todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId);
+    const params = req.params;
+    todos = todos.filter((todoItem) => todoItem.id !== params.todoId);
     res.status(200).json({ message: 'deleted todo', todos: todos });
 });
 //instead of module.exports we will do
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -7,6 +7,9 @@ import {Todo} from '../model/todo'
 
 const router=Router();
 
+type RequestBody={text:string};
+type RequestParams={todoId:string}
+
 let todos:Todo[]=[];
 
 router.get('/',(req,res,next)=>{
@@ -14,19 +17,22 @@ router.get('/',(req,res,next)=>{
 })
 
 router.post('/todo',(req,res,next)=>{
+    const body=req.body as RequestBody
     const newTodo:Todo={
         id:new Date().toISOString(),
-        text:req.body.text,
+        text:body.text,
     }
     todos.push(newTodo);
     res.status(201).json({message:'todo added',todo:newTodo,todos:todos})
 })
 
 router.put('/todo/:todoId',(req,res,next)=>{
-    const tid=req.params.todoId;
+    const body=req.body as RequestBody;
+    const params=req.params as RequestParams;
+    const tid=params.todoId;
     const todoIndex=todos.findIndex((todoItem)=> todoItem.id===tid);
     if(todoIndex>=0){
-        todos[todoIndex]={id:todos[todoIndex].id,text:req.body.text}
+        todos[todoIndex]={id:todos[todoIndex].id,text:body.text}
         //we write return bcoz we want to return from here and dont want to send the res of else block
         return res.status(200).json({message:'updated todo',todos:todos})
     }
@@ -35,7 +41,8 @@ router.put('/todo/:todoId',(req,res,next)=>{
 
 router.delete('/todo/:todoId',(req,res,next)=>{
     //here we will filter out the todo which we want to delete 
-    todos=todos.filter((todoItem)=>todoItem.id!==req.params.todoId)
+    const params=req.params as RequestParams;
+    todos=todos.filter((todoItem)=>todoItem.id!==params.todoId)
     res.status(200).json({message:'deleted todo',todos:todos})
 })
 //instead of module.exports we will do
